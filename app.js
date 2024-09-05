ac71376373c5a976f59b2dc989507a33b7550f09
victorhtl <vht.luna@gmail.com>
2024-09-05 15:22:17 -0300

    firts

--- app.js
+++ app.js
@@ -7,19 +7,14 @@ var routes = require('./routes');
 const connection = require('./config/database');
 
 // Package documentation - https://www.npmjs.com/package/connect-mongo
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongo');
 
 // Need to require the entire Passport config module so app.js knows about it
 require('./config/passport');
 
-/**
- * -------------- GENERAL SETUP ----------------
- */
-
 // Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
 require('dotenv').config();
 
-// Create the Express application
 var app = express();
 
 app.use(express.json());
@@ -30,7 +25,21 @@ app.use(express.urlencoded({extended: true}));
  * -------------- SESSION SETUP ----------------
  */
 
-// TODO
+const sessionStore = MongoStore.create({
+    mongoUrl: process.env.DB_STRING,
+    collectionName: 'sessions',
+    autoRemove: 'native',
+})
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}))
 
 /**
  * -------------- PASSPORT AUTHENTICATION ----------------
@@ -47,10 +56,4 @@ app.use(passport.session());
 // Imports all of the routes from ./routes/index.js
 app.use(routes);
 
-
-/**
- * -------------- SERVER ----------------
- */
-
-// Server listens on http://localhost:3000
-app.listen(3000);
+app.listen(3000, ()=>console.log('Running'));
